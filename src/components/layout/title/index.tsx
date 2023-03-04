a8f5e76e541e56f716d89b8d4ddd1905ee17b919
WebPerformer <webperformerz@gmail.com>
2023-03-04 16:02:27 -0300

    First Commit

--- src/components/layout/title/index.tsx
+++ src/components/layout/title/index.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import { useRouterContext, TitleProps } from "@pankod/refine-core";
 import { Button } from "@pankod/refine-mui";
 
-export const Title: React.FC<TitleProps> = ({ collapsed }) => {
-  const { Link } = useRouterContext();
+import { logo, yariga } from "assets";
 
-  return (
-    <Button fullWidth variant="text" disableRipple>
-      <Link to="/">
-        {collapsed ? (
-          <img src="/refine-collapsed.svg" alt="Refine" width="28px" />
-        ) : (
-          <img src="/refine.svg" alt="Refine" width="140px" />
-        )}
-      </Link>
-    </Button>
-  );
+export const Title: React.FC<TitleProps> = ({ collapsed }) => {
+    const { Link } = useRouterContext();
+
+    return (
+        <Button fullWidth variant="text" disableRipple>
+            <Link to="/">
+                {collapsed ? (
+                    <img src={logo} alt="Yariga" width="28px" />
+                ) : (
+                    <img src={yariga} alt="Refine" width="140px" />
+                )}
+            </Link>
+        </Button>
+    );
 };
